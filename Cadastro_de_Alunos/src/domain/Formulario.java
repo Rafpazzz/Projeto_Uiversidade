2cbee9bdda405202f3fd8d74e0f17e7575785b34
Rafael Paz <rafael_mourapaz@hotmail.com>
2025-05-23 17:19:12 -0300

    alteracoes

--- Cadastro_de_Alunos/src/domain/Formulario.java
+++ Cadastro_de_Alunos/src/domain/Formulario.java
@@ -6,8 +6,10 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Formulario {
+    List<Aluno> listAlunos = new ArrayList<Aluno>();
+
+
     public static void main(String[] args) {
-        List<Aluno> Escola1 = new ArrayList<Aluno>();
         Aluno aluno = new Aluno();
         int escolha;
         Scanner r = new Scanner(System.in);
@@ -15,9 +17,10 @@ public class Formulario {
 
         do {
             System.out.println("Formulario de alunos");
-            System.out.println("[1].add Aluno");
-            System.out.println("[2].mostrar Aluno");
+            System.out.println("[1].Add Aluno");
+            System.out.println("[2].Mostrar Alunos");
             System.out.println("[3].Remover Aluno");
+            System.out.println("[4].Aluno mais velho e mais novo");
             System.out.println("Escolha : ");
             escolha = r.nextInt();
             r.nextLine();
@@ -26,8 +29,8 @@ public class Formulario {
                 case 1:
                     try (FileWriter fw = new FileWriter(file, true); BufferedWriter bw = new BufferedWriter(fw)) {
                         aluno.inscreverAluno();
-                        if (!Escola1.contains(aluno)) {
-                            Escola1.add(aluno);
+                        if (!listAlunos.contains(aluno)) {
+                            listAlunos.add(aluno);
                             bw.write(aluno.toString());
                         } else {
                             throw new RuntimeException("Elemento adicionado");
@@ -39,7 +42,15 @@ public class Formulario {
                     break;
 
                 case 2:
-                    System.out.println(Escola1);
+                    System.out.println("Informe a matricula do Aluno: ");
+                    String m = r.nextLine();
+                    aluno.setMatricula(m);
+                    for(int i = 0; i<listAlunos.size(); i++) {
+                        if(listAlunos.get(i).equals(aluno)){
+                            System.out.println("Aluno: " + listAlunos.get(i));
+                        }
+                    }
+                    System.out.println("Quantidade de alunos no sistema: " + listAlunos.size());
                     break;
 
                 case 3:
@@ -48,12 +59,22 @@ public class Formulario {
                     String matricula;
                     matricula = r.nextLine();
                     aluno.setMatricula(matricula);
-                    remover.removerAluno(Escola1, aluno);
+                    remover.removerAluno(listAlunos, aluno);
                     break;
+
+                case 4:
+                    System.out.println(listAlunos);
                 default:
                     System.out.println("Escolha errada");
             }
 
-        } while (escolha <= 4);
+        } while (escolha <= 5);
+    }
+
+    public static String[] comparacaoIdade(List lista) {
+        String maior, menor;
+        for(int i = 0; i<lista.size(); i++){
+
+        }
     }
 }
